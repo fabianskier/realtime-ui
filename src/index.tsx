f2dbe95d5b722425248c521d651ba00217ca4db7
Oscar Cristaldo <fabianskier@icloud.com>
2022-12-18 20:19:19 -0300

    CO-0001: Add apollo library and display countries
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink, gql,  } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000/api/graphql'
+  })
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
